b7be89a7f7356dbd2e3ce08b8d3cc6f459d90d37
Kalia Xiong <kaliaxyooj@gmail.com>
2024-05-03 22:13:48 -0400

    addded CSS , html codes, and started on some js code

---
+++ weekend-salary-calculator/public/client.js
@@ -0,0 +1,38 @@
+console.log('hello world');
+let newEmployee =[];
+
+function submitForm(event) {
+    console.log('submitForm');
+    // Stop the page from refreshing
+    event.preventDefault();
+
+    let firstName= document.querySelector('#first-name');
+    console.log('first name',firstName);
+    let lastName= document.querySelector('#last-name');
+    console.log('last name',lastName);
+    let idNumber= document.querySelector('#id-number').value;
+    console.log('id number',idNumber);
+    let jobTitle= document.querySelector('#jobTitle');
+    console.log('title',jobTitle);
+    let salary= document.querySelector('#salary').value;
+    console.log('salary',salary);
+
+    let employeeTable = document.querySelector('#employees');
+ 
+    employeeTable.innerHTML += `
+        <tr>
+            <td>${firstName}</td>
+            <td>${lastName}</td>
+            <td>${idNumber}</td>
+            <td>${jobTitle}</td>
+            <td>${salary}</td>
+            <td><button onClick = "deleteRow(event)">Delete</button></td>
+        </tr>
+    `;
+ 
+
+
+}
+function deleteRow(event){
+    event.target.parentElement.parentElement.remove();
+}
